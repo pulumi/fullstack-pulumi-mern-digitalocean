eb9c3086e3ccd6ab7d9792a03805f977426c3389
Christian Nunciato <c@nunciato.org>
2022-01-25 17:43:13 -0800

    Initial

---
+++ infra/index.ts
@@ -0,0 +1,85 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as digitalocean from "@pulumi/digitalocean";
+
+const region = digitalocean.Region.SFO3;
+
+const site = new digitalocean.App("site", {
+    spec: {
+        name: "spec-name",
+        region: region,
+        staticSites: [{
+            buildCommand: "cp -R ./client ./public",
+            git: {
+                branch: "main",
+                repoCloneUrl: "https://github.com/cnunciato/cnunciato/mern-on-do-test.git",
+            },
+            name: "spec-staticsites-name",
+            outputDir: "/public",
+        }],
+    },
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import * as pulumi from "@pulumi/pulumi";
+// import * as digitalocean from "@pulumi/digitalocean";
+
+// To use Spaces, you need to create access keys.
+// Add this to the Registry docs for DO. It's in the README.
+// https://github.com/pulumi/pulumi-digitalocean#configuration
+
+// const region = digitalocean.Region.SFO3;
+
+// This will create a publicly accessible cluster, and it takes a while. But .. neat!
+// Also, to connect from your local machine, you'll need to download a cert from the dashboard.
+// https://docs.digitalocean.com/products/databases/mongodb/how-to/connect/#connect-to-the-database
+// const db = new digitalocean.DatabaseCluster("mongodb-example", {
+//     engine: "mongodb",
+//     nodeCount: 1,
+//     region,
+//     size: digitalocean.DatabaseSlug.DB_1VPCU1GB,
+//     version: "4",
+// });
+
+// const bucket = new digitalocean.SpacesBucket("bucket", {
+//     region: digitalocean.Region.SFO3,
+//     forceDestroy: true,
+// });
+
+// const home = new digitalocean.SpacesBucketObject("index.html", {
+//     bucket: bucket.id,
+//     acl: "public-read",
+//     region,
+//     key: "index.html",
+//     content: `<html>
+//         <h1>Hey!</h1>
+//     <html>`,
+//     contentType: "text/html",
+// });
+
+// export const { host, port, user, password, database } = db;
+// export const url = pulumi.interpolate`https://${bucket.id}.${region}.digitaloceanspaces.com/index.html`
+
+// const registry = new digitalocean.ContainerRegistry("registry", {
+//     subscriptionTierSlug: "starter",
+// });
+
+// const app = new digitalocean.App("app", {
+//     spec:
+// })
+
+// mongosh --tls --tlsCAFile ~/Downloads/ca-certificate.crt \
+//     "mongodb+srv://$(pulumi stack output user):$(pulumi stack output password --show-secrets)@$(pulumi stack output host)/$(pulumi stack output database)" \
