1196b2c6c9f4868db76c29d720bf46a4138ae82a
Christian Nunciato <c@nunciato.org>
2022-01-26 17:40:25 -0800

    Use ambient cert info

--- api/server.js
+++ api/server.js
@@ -8,12 +8,22 @@ const HOST = '0.0.0.0';
 
 const app = express();
 
+let sslCA = path.resolve("./ca-certificate.crt");
+
+if (process.env.CA_CERT) {
+  fs.writeFileSync(sslCA, process.env.CA_CERT);
+}
+
 app.get('/', async (req, res) => {
-    const conn = process.env.DB_CONNECTION_STRING;
-    const client = new mongodb.MongoClient(conn);
+    // const conn = process.env.DB_CONNECTION_STRING;
+    const conn = process.env.DATABASE_URL;
+    const client = new mongodb.MongoClient(conn, {
+        sslCA,
+    });
 
     await client.connect();
     const dbs = await client.db().admin().listDatabases();
+
     res.send(JSON.stringify(dbs));
 });
 
