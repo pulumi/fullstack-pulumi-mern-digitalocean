05cef0fc7be7ad3586121f218b93151d3d63daff
Christian Nunciato <c@nunciato.org>
2022-01-27 18:06:35 -0800

    Bit of cleanup

--- infra/index.ts
+++ infra/index.ts
@@ -2,12 +2,8 @@ import * as pulumi from "@pulumi/pulumi";
 import * as digitalocean from "@pulumi/digitalocean";
 
 const fetch = require("node-fetch");
-
 const region = digitalocean.Region.SFO3;
 
-// This will create a publicly accessible cluster, and it takes a while. But .. neat!
-// Also, to connect from your local machine, you'll need to download a cert from the dashboard.
-// https://docs.digitalocean.com/products/databases/mongodb/how-to/connect/#connect-to-the-database
 const cluster = new digitalocean.DatabaseCluster("mongodb-example", {
     engine: "mongodb",
     version: "4",
@@ -20,9 +16,6 @@ const db = new digitalocean.DatabaseDb("db", {
     clusterId: cluster.id,
 });
 
-// Maybe I need to attach the DB to the app somehow now? How do I do that?
-
-// https://docs.digitalocean.com/products/app-platform/references/app-specification-reference/
 const app = new digitalocean.App("my-whole-entire-app", {
     spec: {
         name: "my-super-sweet-fullstack-app",
@@ -47,22 +40,14 @@ const app = new digitalocean.App("my-whole-entire-app", {
                 runCommand: "npm start",
                 instanceCount: 1,
                 envs: [
-                    // {
-                    //     key: "DB_CONNECTION_STRING",
-                    //     scope: "RUN_AND_BUILD_TIME",
-                    //     value: pulumi.interpolate`mongodb+srv://${cluster.user}:${cluster.password}@${cluster.host}/${cluster.database}`,
-                    //     type: "SECRET",
-                    // },
                     {
                         key: "CA_CERT",
                         scope: "RUN_AND_BUILD_TIME",
-                        // value: pulumi.interpolate`\${${cluster.name}.CA_CERT}`,
                         value: "${db.CA_CERT}",
                     },
                     {
                         key: "DATABASE_URL",
                         scope: "RUN_AND_BUILD_TIME",
-                        // value: pulumi.interpolate`\${${cluster.name}.DATABASE_URL}`,
                         value: "${db.DATABASE_URL}",
                     }
                 ],
@@ -87,16 +72,6 @@ const app = new digitalocean.App("my-whole-entire-app", {
 // This feels super gross to have to do this, but.
 pulumi.all([app.id, app.spec, cluster.name, cluster.engine]).apply(async ([ appID, appSpec, clusterName, clusterEngine]) => {
 
-    // Now overwrite the databases bit so we can update it manually.
-    // const newSpec = Object.assign({}, appSpec);
-
-    // const result = await fetch("https://api.digitalocean.com/v2/apps", {
-    //     headers: {
-    //         "Authorization": `Bearer ${process.env.DIGITALOCEAN_TOKEN}`,
-    //         "Content-Type": "application/json",
-    //     },
-    // });
-
     const newApp = await fetch(`https://api.digitalocean.com/v2/apps/${appID}`, {
         method: "GET",
         headers: {
@@ -106,22 +81,16 @@ pulumi.all([app.id, app.spec, cluster.name, cluster.engine]).apply(async ([ appI
     });
 
     const newAppResult = await newApp.json();
-
     const newAppSpec = await newAppResult.app.spec;
-
-    console.log({ newAppSpec });
 
     newAppSpec.databases = [
         {
             name: "db",
             clusterName,
             engine: clusterEngine.toUpperCase(),
-            // version: 4,
             production: true,
         }
     ];
-
-    console.log({ newAppSpec });
 
     const result = await fetch(`https://api.digitalocean.com/v2/apps/${appID}`, {
         method: "PUT",
@@ -131,15 +100,7 @@ pulumi.all([app.id, app.spec, cluster.name, cluster.engine]).apply(async ([ appI
             "Content-Type": "application/json",
         },
     });
-
-    console.log(await result.json());
 });
-
-// - cluster_name: mongodb-example-19ef379
-//   engine: MONGODB
-//   name: mongodb-example-19ef379
-//   production: true
-//   version: "4"
 
 const trust = new digitalocean.DatabaseFirewall("firewall", {
     clusterId: cluster.id,
@@ -154,5 +115,3 @@ const trust = new digitalocean.DatabaseFirewall("firewall", {
 export const { host, port, user, password, database } = cluster;
 export const { name } = db;
 export const { liveUrl } = app;
-
-// mongosh --tls --tlsCAFile ~/Downloads/ca-certificate.crt "mongodb+srv://$(pulumi stack output user):$(pulumi stack output password --show-secrets)@$(pulumi stack output host)/$(pulumi stack output database)"
